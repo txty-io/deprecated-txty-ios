315e0a3d9a4fc6e4ceb6d7f8372f6b2fab246e67
Lyn Al Masri <lyn.almasri@allaboutapps.at>
2020-12-08 20:30:54 +0100

    bug fixes

--- texterify-ios-sdk/Downloader.swift
+++ texterify-ios-sdk/Downloader.swift
@@ -21,23 +21,25 @@ class Downloader {
         Downloader.dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
     }
 
-    func downloadLocalizationBundle(completion: @escaping (TexterifyError?) -> Void) {
+    func downloadLocalizationBundle(successCompletionHandler:@escaping() -> (), errorCompletionHandler: @escaping (TexterifyError?) -> Void) {
         guard let appLanguageCode = Locale.current.languageCode, let regionCode = Locale.current.regionCode else {
             return
         }
-        let locale = "\(appLanguageCode)_\(regionCode)"
-        let timeStamp = Downloader.dateFormatter.string(from: Date())
+        let locale = "\(appLanguageCode)-\(regionCode)"
         var components = URLComponents()
         components.scheme = "https"
         components.host = baseUrl
         components.path = "/api/v1/projects/\(projectId)/export_configs/\(exportConfigId)/release"
         components.queryItems = [
-            URLQueryItem(name: "locale", value: "\(locale)"),
-            URLQueryItem(name: "timestamp", value: timeStamp)
+            URLQueryItem(name: "locale", value: "\(locale)")
         ]
+        let userDefault = UserDefaults.standard
+        if let timeStamp = userDefault.string(forKey: "texterify_timeStamp") {
+            components.queryItems?.append(URLQueryItem(name: "timestamp", value: timeStamp))
+        }
 
         guard let url = components.url else {
-            completion(.InvalidURL)
+            errorCompletionHandler(.invalidURL)
             return
         }
         let sessionConfig = URLSessionConfiguration.default
@@ -45,12 +47,11 @@ class Downloader {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         let task = session.dataTask(with: request, completionHandler: { data, response, error in
-            if let error = error {
-                print("There was an error downloading the file: \(error.localizedDescription)\n")
+            if error != nil {
+                errorCompletionHandler(.downloadError)
             } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
                 guard let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
                 let finalFile = documentsUrl.appendingPathComponent("strings.json")
-
                 let destinationUrl = documentsUrl.appendingPathComponent(url.lastPathComponent)
                 if let _ = try? data.write(to: destinationUrl) {
                     do {
@@ -58,9 +59,9 @@ class Downloader {
                             try FileManager.default.removeItem(at: finalFile)
                         }
                         try FileManager.default.moveItem(at: destinationUrl, to: finalFile)
-                        completion(nil)
+                        successCompletionHandler()
                     } catch {
-                        completion(.ErrorWritingToFile)
+                        errorCompletionHandler(.errorWritingToFile)
                     }
                 }
             }
--- texterify-ios-sdk/TexterifyManager.swift
+++ texterify-ios-sdk/TexterifyManager.swift
@@ -11,11 +11,12 @@ import Foundation
 public class TexterifyManager {
     static let bundleName = "TexterifyLocalization.bundle"
     static let suffix = ".lproj"
-    let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-    var baseUrl = ""
-    var projectId = ""
-    var exportConfigId = ""
-    var complitionHandler: ((String?, TexterifyError?) -> Void)?
+    public static var timeStamp = ""
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    private var baseUrl = ""
+    private var projectId = ""
+    private var exportConfigId = ""
+    private var complitionHandler: ((TexterifyError?) -> Void)?
 
     public init(baseUrl: String, projectId: String, exportConfigId: String) {
         self.baseUrl = baseUrl
@@ -23,16 +24,10 @@ public class TexterifyManager {
         self.exportConfigId = exportConfigId
     }
 
-    public func getUpdatedStrings( complitionHandler: @escaping (String?, TexterifyError?) -> Void) {
+    public func getUpdatedStrings( complitionHandler: @escaping (TexterifyError?) -> Void) {
         self.complitionHandler = complitionHandler
         let downloader = Downloader(baseUrl: self.baseUrl, projectId: self.projectId, exportConfigId: self.exportConfigId)
-        downloader.downloadLocalizationBundle { [weak self] (error) in
-            if error != nil {
-                complitionHandler( nil, error)
-            } else {
-                self?.parseData()
-            }
-        }
+        downloader.downloadLocalizationBundle(successCompletionHandler: parseData, errorCompletionHandler: complitionHandler)
     }
 
     func parseData() {
@@ -44,9 +39,12 @@ public class TexterifyManager {
             let decoder = JSONDecoder()
             let data = try Data(contentsOf: jsonFile)
             let jsonData = try decoder.decode(Model.self, from: data)
+            TexterifyManager.timeStamp = jsonData.timestamp
+            let defaults = UserDefaults.standard
+            defaults.setValue(jsonData.timestamp, forKey: "texterify_timeStamp")
             self.createStringFiles(jsonData: jsonData)
         } catch {
-            complitionHandler?(nil, .DecodingError)
+            complitionHandler?(.decodingError)
         }
     }
 
@@ -59,7 +57,7 @@ public class TexterifyManager {
             if Bundle(path: "\(documentDirectory.path)/\(TexterifyManager.bundleName)") == nil {
                 try FileManager.default.createDirectory(at: documentDirectory.appendingPathComponent(TexterifyManager.bundleName), withIntermediateDirectories: true, attributes: nil)
             }
-            
+    
             let localizationFolderURL = documentDirectory.appendingPathComponent(TexterifyManager.bundleName).appendingPathComponent(jsonData.data.languageCode + TexterifyManager.suffix)
             if !FileManager.default.fileExists(atPath: localizationFolderURL.path) {
                 try FileManager.default.createDirectory(at: localizationFolderURL, withIntermediateDirectories: true, attributes: nil)
@@ -75,18 +73,23 @@ public class TexterifyManager {
             }
             try stringChange.write(to: localizationFile, atomically: true, encoding: .utf8)
         } catch {
-            complitionHandler?(nil, .ErrorWritingToFile)
+            complitionHandler?(.errorWritingToFile)
         }
     }
 
     public static func localisedString(key: String, tableName: String?, comment: String) -> String {
-        let localizationBundle = Bundle(path: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/\(bundleName)")
-        return NSLocalizedString(key, tableName: nil, bundle: localizationBundle!, value: "", comment: "")
+        if let localizationBundle = Bundle(path: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/\(bundleName)") {
+            return NSLocalizedString(key, tableName: nil, bundle: localizationBundle, value: "", comment: comment)
+        } else {
+            return NSLocalizedString(key, comment: comment)
+        }
+        
     }
 }
 
 public enum TexterifyError: Error {
-    case DecodingError
-    case InvalidURL
-    case ErrorWritingToFile
+    case decodingError
+    case invalidURL
+    case errorWritingToFile
+    case downloadError
 }
